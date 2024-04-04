6c0194ba997fdea937e546653e69c90186cbb582
Gabriel Santana <130417936+GabrielSantana013@users.noreply.github.com>
2024-04-03 21:07:07 -0300

    Update Vetores_e_Matrizes.cpp

--- Vetores_e_Matrizes.cpp
+++ Vetores_e_Matrizes.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 
-#define ex23
+#define ex24
+//refazer o 24 e verificar o 23
 
 #ifdef ex01
 
@@ -1450,12 +1451,41 @@ int main()
 
 /*
 
+Escreva um algoritmo que leia um inteiro N (0 ≤ N ≤ 100), correspondente a ordem de uma matriz M de inteiros,
+e construa a matriz de acordo com o exemplo abaixo.
+
+Entrada
+A entrada consiste de vários inteiros, um valor por linha, correspondentes as ordens das matrizes a serem construídas.
+O final da entrada é marcado por um valor de ordem igual a zero (0).
+
+Saída
+Para cada inteiro da entrada imprima a matriz correspondente, de acordo com o exemplo.
+Os valores das matrizes devem ser formatados em um campo de tamanho 3 justificados à direita e separados por espaço.
+Após o último caractere de cada linha da matriz não deve haver espaços em branco.
+Após a impressão de cada matriz deve ser deixada uma linha em branco.
+
 */
 
 using namespace std;
 
 int main()
 {
+    int N, matriz[100][100];
+    cin>>N;
+
+    while(N != 0)
+    {
+        for(int i = 0; i <N; i++)
+        {
+            for(int j = 0; j <N; j++)
+            {
+                matriz[i][j] = 1;
+                cout<<matriz[i][j];
+            }
+            cout<<endl;
+        }
+    cin>>N;
+    }
 
     return 0;
 }
