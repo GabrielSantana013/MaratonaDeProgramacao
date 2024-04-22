f3316093133352879de114499410539dfa12b6e0
Gabriel Santana <130417936+GabrielSantana013@users.noreply.github.com>
2024-04-22 12:01:37 -0300

    Update STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <bits/stdc++.h> //importa tudo (stl, string, set, map)
 
-#define ex07
+#define ex09
 //pulou o 2
 //pulou o 5
+//pulou o 8
 
 #ifdef ex01
 
@@ -459,7 +460,7 @@ int main()
             cin>>numeros[i];
         }
 
-        sort(numeros.begin(), numeros.end());
+        sort(numeros.begin(), numeros.end()); // arruma o vetor
 
         for(int i = 0; i < N-1; i++)
         {
@@ -469,11 +470,15 @@ int main()
            {
                if(telefone[j] == numeros[i+1][j])
                {
-                   cout<<"cu";
                    contador++;
+               }
+               else
+               {
+                   break;
                }
            }
         }
+        cout<<contador<<endl;
     }
 
 
@@ -503,16 +508,90 @@ int main()
 
 /*
 
+Cada funcionário de um serviço burocrático tem uma descrição do cargo - alguns parágrafos que descrevem as responsabilidades do trabalho.
+A descrição do cargo combinado com outros fatores, como por exemplo tempo de serviço, é utilizado para determinar qual é o salário deste funcionário.
 
+Um sistema denominado Pontos de Feno (Hay Points) libera o departamento de Recursos Humanos de ter que fazer um
+julgamento inteligente do valor de cada empregado para a empresa.
+A descrição de um cargo ou função é feita através da verificação de palavras e frases que indicam responsabilidade.
+Em particular, descrições de cargo que indicam o
+controle sobre um grande orçamento ou gestão sobe um grande número de pessoas geram escores altos neste sistema.
+
+Você deve implementar um sistema de Ponto de Feno simplificado. Você terá como informações um dicionário
+Hay Point que conterá algumas palavras-chaves que são as descrições dos cargos e um valor em dólares americanos associado com cada um destes cargos.
+Para cada descrição de trabalho você deverá calcular o salário associado com o trabalho, de acordo com este sistema.
+
+Entrada
+A entrada contém vários casos de teste. A primeira linha da entrada contém dois números inteiros positivos: M (M ≤ 1000),
+que é o número de palavras no dicionário Hay Point, e um número inteiro N (N ≤ 100) que corresponde à quantidade de descrições de cargos ou funções.
+M linhas seguem, cada um contém uma palavra (uma seqüência de até 16 letras minúsculas) e um valor de dólar (um número real entre 0 e 1000000).
+Logo na sequência, após o dicionário, estão as descrições de cada uma dos cargos N.
+
+Cada descrição de cargo consiste em uma ou mais linhas de texto. Para sua conveniência, o texto contém somente letras minúsculas (de 'a' até 'z').
+Cada descrição de cargo é finalizada por uma linha contendo um ponto ".".
+
+Saída
+Para cada caso de teste de entrada, imprima o salário do funcionário que é calculado através deste sistema Pontos de Feno
+(que nada mais é do que a soma do valor de todas as palavras que aparecem na descrição do cargo).
+Obs.: o valor das palavras que não aparecem no dicionário é zero (0).
 
 */
 
 using namespace std;
 int main()
 {
+    int M, N, contador = 0;
+    cin>>M >> N;
+
+    map<string, int> hayPoint;
+
+    for(int i = 0; i < M; i++)
+    {
+        string chave; cin>>chave;
+        int valor; cin>>valor;
+        hayPoint.insert({chave, valor});
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        string palavra;
+        while(cin >> palavra && palavra != ".")
+        {
+            contador+=hayPoint[palavra];
+        }
+        cout<<contador<<endl;
+        contador = 0;
+    }
 
     return 0;
 }
+// if (hayPoint.find(palavra) != hayPoint.end())
+/*   vector<tuple<int, int, int>> gabriel;
+
+    for (auto [a, b, c] : gabriel)
+    {
+
+    }
+  */
+
+
+
+    /*for (auto [chave, valor] : hayPoint)
+    {
+        cout << chave << endl;
+        cout << valor << endl;
+    }*/
+
+    /*for (auto p : hayPoint)
+    {
+        cout << p.first << endl;
+        cout << p.second << endl;
+    }*/
+    /*for (pair<string, int> p : hayPoint)
+    {
+        cout << p.first << endl;
+        cout << p.second << endl;
+    }*/
 #endif
 
 
