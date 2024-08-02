23acad4ffd305ce7d21eb8573ab131cc6c9e51ad
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2024-08-02 16:27:50 -0300

    Exercícios Concluídos

---
+++ Lista-BeeCrowd/1759HoHoHo.cpp
@@ -0,0 +1,30 @@
+/*
+Papai Noel está brincando com seus duendes para entretê-los durante a véspera do Natal.
+A brincadeira consiste nos elfos escreverem números em pedaços de papel e colocarem no gorro do Papai Noel. 
+Após todos terminarem de colocar os números Noel sorteia um papel e aquele número representa quantos "Ho" o Noel deve falar.
+
+Seu trabalho é ajudar o Papai Noel montando um problema que mostre todos os "Ho" que ele deve falar dado o número sorteado.
+
+Entrada
+A entrada é composta por um único inteiro N (0 < N ≤ 106) representando quantos "Ho" serão falados por Noel.
+
+Saída
+A saída é composta por todos "Ho" que Papai Noel deve falar separados por um espaço. Após o último "Ho" deve ser apresentado um "!" 
+encerrando o programa.
+*/
+
+#include <iostream>
+
+int main(){
+
+    int N;
+    std::cin>>N;
+
+    for(int i = 0; i<N; i++)
+    {
+        if(i+1==N){std::cout<<"Ho!"<<std::endl;}
+        else{std::cout<<"Ho ";}
+    }
+
+    return 0;
+}
