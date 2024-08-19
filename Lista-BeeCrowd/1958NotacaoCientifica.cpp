919fc257ceb9e32a16a2c44a4683b7a39427fe9f
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2024-08-19 19:29:56 -0300

    Exercícios Concluídos

---
+++ Lista-BeeCrowd/1958NotacaoCientifica.cpp
@@ -0,0 +1,16 @@
+/*
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    long double a;
+    char s[200];
+    while(scanf("%LE",&a)!=EOF){
+    sprintf(s,"%LE",a);
+    if(s[0]!='-')printf("+");
+    printf("%.4LE\n",a);
+    }
+    return 0;
+}
