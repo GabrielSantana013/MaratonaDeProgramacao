6732cadc5669c5fc2bce457745c0244305e2f1ea
Gabriel Santana <130417936+GabrielSantana013@users.noreply.github.com>
2025-02-11 11:38:44 -0300

    fix: incompleto dado.cpp

---
+++ Lista-FEI/Structs/1342Dado.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define dbg(x) cout << #x << " = " << x << endl
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+using namespace std;
+
+void solution(int players, int quadrados){
+
+	int t1,t2,t3; cin>>t1,t2,t3;
+	int nRolagem; cin>>nRolagem;
+	
+	for(int i = 0; i < nRolagem; i++)
+	{
+		
+	}
+}
+
+
+int main(){
+    _;
+
+	int players, quadrados;
+	while(cin>>players>>quadrados)
+		solution(players, quadrados);
+	
+
+    return 0;
+}
