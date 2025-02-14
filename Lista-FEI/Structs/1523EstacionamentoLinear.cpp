99c1a05982991a03410d12575a02aa18b9a2d9ce
Gabriel Santana <130417936+GabrielSantana013@users.noreply.github.com>
2025-02-14 12:17:13 -0300

    feat: exercício concluido-

--- Lista-FEI/Structs/1523EstacionamentoLinear.cpp
+++ Lista-FEI/Structs/1523EstacionamentoLinear.cpp
@@ -17,13 +17,49 @@ void solution(){
 		cin>>n>>k;
 		if(n == 0 and k ==0) break;
 
-		vector<pair<int,int>> estacionamento; //chegada e saida;
-		
+		vector<pair<int,int>> estacionamento, carros; //chegada e saida;
+
 		for(int i = 0; i < n; i++){
 		
 			int ci,si; cin>>ci>>si;
-			estacionamento.emplace_back(ci, si);
+			carros.emplace_back(ci, si);
 		}
+
+	
+		bool liberado = true;
+		for(int i = 0; i < n; i ++)
+		{
+			int ci = carros[i].f;
+			int si = carros[i].s;
+
+		
+			for(int j = estacionamento.size()-1; j>=0; j--){
+
+				if(ci >= estacionamento[j].f && ci < estacionamento[j].s && si > estacionamento[j].s){
+
+					liberado = false;
+					break;
+				}
+			}
+
+			if(!liberado) break;
+
+			while(!estacionamento.empty() && estacionamento.back().second <=ci)
+			{
+				estacionamento.pop_back();
+			}
+			estacionamento.emplace_back(ci,si);
+			
+			if(estacionamento.size()>k){liberado = false; break;}
+
+		}
+
+	
+		//for(auto i: estacionamento)
+		//	cout<<i.f<<" "<<i.s<<endl;
+
+
+	if(liberado) cout<<"Sim"<<endl; else cout<<"Não"<<endl;
 
 	}
 
